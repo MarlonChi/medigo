51624512d223151fdba424fdba3fd3b9ce2c814c
MarlonChi <marlonchiodelli@hotmail.com>
2024-10-09 19:37:19 -0300

    feat: create appointment

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -24,3 +24,17 @@ export const createAppointment = async (
     console.error(err);
   }
 };
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
+  } catch (err) {
+    console.error(err);
+  }
+};
